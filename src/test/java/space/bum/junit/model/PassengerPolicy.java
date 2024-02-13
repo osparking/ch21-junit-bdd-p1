bd5402254e7d6bebcd274a1a58cd983b61c71637
osparking <jbpark03@gmail.com>
2024-02-13 22:00:33 +0900

    오이 특색 파일로 만들어진 시험 뼈대

---
+++ src/test/java/space/bum/junit/model/PassengerPolicy.java
@@ -0,0 +1,99 @@
+package space.bum.junit.model;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+class PassengerPolicy {
+
+  @Given("there is an economy flight")
+  public void there_is_an_economy_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("we have a regular passenger")
+  public void we_have_a_regular_passenger() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you can add and remove him from an economy flight")
+  public void you_can_add_and_remove_him_from_an_economy_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add a regular passenger to an economy flight more than once")
+  public void you_cannot_add_a_regular_passenger_to_an_economy_flight_more_than_once() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("we have a VIP passenger")
+  public void we_have_a_vip_passenger() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you can add him but cannot remove him from an economy flight")
+  public void you_can_add_him_but_cannot_remove_him_from_an_economy_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add a VIP passenger to an economy flight more than once")
+  public void you_cannot_add_a_vip_passenger_to_an_economy_flight_more_than_once() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("there is a business flight")
+  public void there_is_a_business_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add or remove him from a business flight")
+  public void you_cannot_add_or_remove_him_from_a_business_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you can add him but cannot remove him from a business flight")
+  public void you_can_add_him_but_cannot_remove_him_from_a_business_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add a VIP passenger to a business flight more than once")
+  public void you_cannot_add_a_vip_passenger_to_a_business_flight_more_than_once() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("there is a premium flight")
+  public void there_is_a_premium_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add or remove him from a premium flight")
+  public void you_cannot_add_or_remove_him_from_a_premium_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you can add and remove him from a premium flight")
+  public void you_can_add_and_remove_him_from_a_premium_flight() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("you cannot add a VIP passenger to a premium flight more than once")
+  public void you_cannot_add_a_vip_passenger_to_a_premium_flight_more_than_once() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+}
