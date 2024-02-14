6ef7edf770e4690391665a65d72f98b995365fc4
osparking <jbpark03@gmail.com>
2024-02-14 18:38:38 +0900

    PassengerPolicy 클래스 public 으로 수정

--- src/test/java/space/bum/junit/model/PassengerPolicy.java
+++ src/test/java/space/bum/junit/model/PassengerPolicy.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-class PassengerPolicy {
+public class PassengerPolicy {
   private Flight economyFlight;
   private Passenger kim;
   private Flight businessFlight;
