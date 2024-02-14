2042d9ad0e7048fc074fca172e6496ebb92433dd
osparking <jbpark03@gmail.com>
2024-02-14 17:53:47 +0900

    시험 클래스 접근 수준 public 으로 확대

--- src/test/java/space/bum/junit/model/CucumberTest.java
+++ src/test/java/space/bum/junit/model/CucumberTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty" }, features = "classpath:features")
-class CucumberTest {
+public class CucumberTest {
   /**
    * 비어있어야 함. 단계 정의는 다른 클래스에 둬야 됨.
    */
