a0f00ea1aa91f393e839261a829febd862f74e59
osparking <jbpark03@gmail.com>
2024-02-14 11:38:42 +0900

    오이 시험을 위한 특별한 클라스

---
+++ src/test/java/space/bum/junit/model/CucumberTest.java
@@ -0,0 +1,14 @@
+package space.bum.junit.model;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, features = "classpath:features")
+class CucumberTest {
+  /**
+   * 비어있어야 함. 단계 정의는 다른 클래스에 둬야 됨.
+   */
+}
